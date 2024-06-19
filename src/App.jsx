5aed1958501949cc18d7f75c4aa0f527854d377a
sailingsam <jainsaksham1004@gmail.com>
2024-06-20 02:13:44 +0530

    started with nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import { useState } from "react";
-import "./Prelaunch";
-import Prelaunch from "./Prelaunch";
+import Navheader from "./Components/NavHeader/Navheader";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div>
-      {/* <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <div className="w-1"> */}
-        <Prelaunch />
-      {/* </div> */}
+      <Navheader />
     </div>
   );
 }
