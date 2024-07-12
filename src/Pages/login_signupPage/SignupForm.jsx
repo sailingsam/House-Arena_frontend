5f0973c78bac5c6ac126f10e3ba27142a7c5037f
sailingsam <jainsaksham1004@gmail.com>
2024-07-12 16:20:44 +0530

    added antd  to package.json

--- src/Pages/login_signupPage/SignupForm.jsx
+++ src/Pages/login_signupPage/SignupForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-// import { RegisterUser } from "../../backendCalls/user";
+// import { RegisterUser } from "../../backendCalls/user";  
 import {
   AutoComplete,
   Button,
