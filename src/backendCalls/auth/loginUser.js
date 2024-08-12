4b7b988777399ea894c3d9dd7592bed703aab891
sailingsam <jainsaksham1004@gmail.com>
2024-08-13 03:13:57 +0530

    updating links

--- src/backendCalls/auth/loginUser.js
+++ src/backendCalls/auth/loginUser.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const loginUser = async (values) => {
     try {
-        const res = await axios.post("http://localhost:3999/api/users/login", values, {
+        const res = await axios.post(`${import.meta.env.VITE_BASE_URL_BACKEND}/api/users/login`, values, {
             headers: {
                 "Content-Type" : "application/json"
             }
