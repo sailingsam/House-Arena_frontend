29eafcd2f6661e1c1da58449b73f8de86903c443
sailingsam <jainsaksham1004@gmail.com>
2024-08-13 03:09:09 +0530

    ready to implement admin features

--- src/backendCalls/auth/registerUser.js
+++ src/backendCalls/auth/registerUser.js
@@ -1,18 +1,3 @@
-// import axios from "axios";
-
-// export const RegisterUser = async (values) => {
-//     try {
-//         const res = await axios.post(`${import.meta.env.VITE_BASE_URL_BACKEND}/api/users/register`, values, {
-//             Headers: {
-//                 "Content-Type": "application/json",
-//             },
-//         });
-//         return res.data;
-//     } catch (error) {
-//         console.log('error in registerUser', error.response.data);  
-//         return error.response.data;
-//     }
-// };
 import axios from "axios";
 
 export const RegisterUser = async (values) => {
